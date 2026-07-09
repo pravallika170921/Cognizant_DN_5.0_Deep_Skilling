9e888b66abe04862b5d1dd38221bd689e9aeeef2
KOTHURI PRAVALLIKA <pravikothuri1947@gmail.com>
2026-07-09 16:54:01 +0530

    Add files via upload

---
+++ week_5/3.React/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div className="App">
+
+      <CalculateScore
+        Name="John"
+        School="ABC Public School"
+        Total={450}
+        goal={5}
+      />
+
+    </div>
+  );
+}
+
+export default App;
