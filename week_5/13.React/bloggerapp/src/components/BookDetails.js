b7d58f57d7d4d75045526bbcb707416c52448e01
KOTHURI PRAVALLIKA <pravikothuri1947@gmail.com>
2026-07-11 21:08:54 +0530

    Add files via upload

---
+++ week_5/13.React/bloggerapp/src/components/BookDetails.js
@@ -0,0 +1,19 @@
+function BookDetails(props) {
+
+  return (
+    <div className="box">
+
+      <h1>Book Details</h1>
+
+      {props.books.map((book) => (
+        <div key={book.id}>
+          <h3>{book.bname}</h3>
+          <h4>{book.price}</h4>
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default BookDetails;
